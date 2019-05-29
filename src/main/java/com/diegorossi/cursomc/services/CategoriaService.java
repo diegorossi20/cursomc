131a6ca57c4d97567e1d0c463467062015cdd256
Diego Rossi <diegorossi20@gmail.com>
2019-05-29 17:51:15 -0300

    Validacao sintatica com bean validation

--- src/main/java/com/diegorossi/cursomc/services/CategoriaService.java
+++ src/main/java/com/diegorossi/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.diegorossi.cursomc.domain.Categoria;
+import com.diegorossi.cursomc.dto.CategoriaDTO;
 import com.diegorossi.cursomc.repositories.CategoriaRepository;
 import com.diegorossi.cursomc.services.exceptions.DataIntegrityException;
 import com.diegorossi.cursomc.services.exceptions.ObjectNotFoundException;
@@ -53,5 +54,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(), objDTO.getNome());
 	}
 }
