43e24a63d82ada629cd53efd3b2c58b6e63f315d
Diego Rossi <diegorossi20@gmail.com>
2019-05-30 10:59:18 -0300

    PUT, DELETE e GET para Cliente

--- src/main/java/com/diegorossi/cursomc/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/diegorossi/cursomc/resources/exceptions/ResourceExceptionHandler.java
@@ -33,7 +33,7 @@ public class ResourceExceptionHandler {
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	public ResponseEntity<StandardError> validator(MethodArgumentNotValidException e, HttpServletRequest request) {
 
-		ValidationError err = new ValidationError(HttpStatus.BAD_REQUEST.value(), "Erro de validadção",
+		ValidationError err = new ValidationError(HttpStatus.BAD_REQUEST.value(), "Erro de validação",
 				System.currentTimeMillis());
 		for (FieldError x : e.getBindingResult().getFieldErrors()) {
 			err.addError(x.getField(), x.getDefaultMessage());
