fd394f19de9b4cd07d0a070e18bcb5ab21520b09
Diego Rossi <diegorossi20@gmail.com>
2019-06-03 17:03:34 -0300

    Email HTML

--- src/main/java/com/diegorossi/cursomc/services/EmailService.java
+++ src/main/java/com/diegorossi/cursomc/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.diegorossi.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.diegorossi.cursomc.domain.Pedido;
@@ -8,5 +10,9 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj); 
+	
+	void sendHtmlEmail(MimeMessage msg);
 	
 }
